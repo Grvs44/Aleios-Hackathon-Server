6b7c4e2eb4fc67786e283a7429c3a7a0a761b98e
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-02-11 15:51:13 +0000

    Login working Does logout work?

--- sustainabilityapp/views.py
+++ sustainabilityapp/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import viewsets, generics
 from rest_framework.permissions import AllowAny
 from rest_framework.authentication import SessionAuthentication
@@ -56,6 +56,6 @@ class SignUpView(APIView):
 """
 
 class UserCreateAPIView(generics.CreateAPIView):
-    queryset = User.objects.all()
+    queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
