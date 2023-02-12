26875001e1cde35b2977d1725aef0f4f3871ffbc
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-02-12 08:50:28 +0000

    Serve uploaded images

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static"
 
+MEDIA_ROOT = BASE_DIR / 'uploads/'
+MEDIA_URL = 'uploads/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- sustainabilityapp/urls.py
+++ sustainabilityapp/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 import knox.views
 from .views import *
 from rest_framework.routers import DefaultRouter
@@ -16,6 +18,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path(r'auth/login/', LoginView.as_view(), name='knox_login'),
     path(r'auth/logout/', knox.views.LogoutView.as_view(), name='knox_logout'),
-    path(r'auth/logoutall/', knox.views.LogoutAllView.as_view(), name='knox_logoutall'),
+    path(r'auth/logoutall/', knox.views.LogoutAllView.as_view(),
+         name='knox_logoutall'),
     path('auth/signup/', UserCreateAPIView.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
